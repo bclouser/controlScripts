38bdbbefdae58b42cb75a843c1874ec09b24dbb2
Ben Clouser <bclouse91@gmail.com>
2017-02-19 12:16:47 -0800

    Adding script to symlink scripts to be used on commandline

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+PWD=$(pwd)
+ln -s ${PWD}/lights.py /usr/local/bin/lights
+ln -s ${PWD}/buttonLights.py /usr/local/bin/buttonLights
+ln -s ${PWD}/hdmiSelector.py /usr/local/bin/hdmiSelector
+ln -s ${PWD}/pixelWall.py /usr/local/bin/pixelWall
+ln -s ${PWD}/projector.py /usr/local/bin/projector
