b4535f965cf428c08e06749d9eaa45e3845ef86c
Ben Clouser <bclouse91@gmail.com>
2017-03-01 22:51:53 -0800

    adding script to control reading light

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ ln -s ${PWD}/lights.py /usr/local/bin/lights
 ln -s ${PWD}/buttonLights.py /usr/local/bin/buttonLights
 ln -s ${PWD}/hdmiSelector.py /usr/local/bin/hdmiSelector
 ln -s ${PWD}/pixelWall.py /usr/local/bin/pixelWall
+ln -s ${PWD}/readingLight.py /usr/local/bin/readingLight
 ln -s ${PWD}/projector.py /usr/local/bin/projector
